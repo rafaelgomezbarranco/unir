3a34d56fb8c1a4929b9920ba54901d1c067b0231
Rafael Gomez Barranco <rafael.gomez971@comunidadunir.net>
2025-03-12 18:52:19 +0100

    First POC to send a WhatsApp message

---
+++ NotificationWeb/NotificationConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using RestSharp;
+
+namespace WhatsAppNotificationConsolePOC;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        const string instanceId = "<<instance_id>>"; // Your instance Id
+        const string token = "<<instance_token>>";   // Your instance Token
+        const string mobile = "<<phone_number>>";    // The phone number that sends the message
+        const string message = "Hola, mundo 👋🏻 a través de WhatsApp";
+        const string url = "https://api.ultramsg.com/" + instanceId + "/messages/chat";
+
+        var client = new RestClient(url);
+        var request = new RestRequest(url, Method.Post);
+        request.AddHeader("content-type", "application/x-www-form-urlencoded");
+        request.AddParameter("token", token);
+        request.AddParameter("to", mobile);
+        request.AddParameter("body", message);
+
+        var response = await client.ExecuteAsync(request);
+        var output = response.Content;
+
+        Console.WriteLine(output);
+    }
+}
