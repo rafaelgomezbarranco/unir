5913974df07a66d64260c52ba6f0d1d78f59da11
Rafael Gomez Barranco <rafael.gomez971@comunidadunir.net>
2025-04-15 19:19:15 +0200

    Add documentation with Swagger

---
+++ NotificationWeb/NotificationWebApi/Modules/Swagger/SwaggerExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace NotificationWebApi.Modules.Swagger;
+
+public static class SwaggerExtensions
+{
+    public static IServiceCollection AddSwaggerGenWithDocumentation(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(_ =>
+        {
+            // Created the Swagger document
+            _.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "Version 1.0.0",
+                Title = "Notification Api",
+                Description = " This API offers you the ability to send WhatsApp or SMS messages",
+                TermsOfService = new Uri("https://mexico.unir.net/estudios/condiciones.htm"),
+                License = new OpenApiLicense
+                {
+                    Name = "Professional licence.",
+                    Url = new Uri("https://example.com/license")
+                }
+            });
+
+
+            // Generate the swagger documentation
+            foreach (var name in Directory.GetFiles(AppContext.BaseDirectory, "*.XML", SearchOption.TopDirectoryOnly))
+                _.IncludeXmlComments(name);
+        });
+
+        return services;
+    }
+}
--- NotificationWeb/NotificationWebApi/Program.cs
+++ NotificationWeb/NotificationWebApi/Program.cs
@@ -1,25 +1,32 @@
-var builder = WebApplication.CreateBuilder(args);
+using NotificationWebApi.Modules.Swagger;
 
-// Add services to the container.
+namespace NotificationWebApi;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public static class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+
+        builder.Services.AddSwaggerGenWithDocumentation();
+
+        var app = builder.Build();
+
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
