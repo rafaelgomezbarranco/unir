95c9dbbd10444879ac4448c57412a50478762dee
Rafael Gomez Barranco <rafael.gomez971@comunidadunir.net>
2025-04-30 19:14:25 +0200

    Adjust the notification message

--- NotificationWeb/NotificationWebApi/Business/AppointmentMessageService.cs
+++ NotificationWeb/NotificationWebApi/Business/AppointmentMessageService.cs
@@ -8,14 +8,28 @@ public class AppointmentMessageService : IAppointmentMessageService
     {
         patientName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(patientName.Trim());
 
-        var dateFormatted = dateTime.ToString("f", GetCulture(languageCode));
+        var dateFormatted = FormatDate(dateTime, languageCode);
 
         return languageCode.ToLower() switch
         {
-            "es" => $"Hola {patientName}, su cita médica está programada para el {dateFormatted}.",
-            "en" => $"Hello {patientName}, your medical appointment is scheduled for {dateFormatted}.",
+            "es" => $"Hola {patientName}, \nSu cita médica está programada para el {dateFormatted}.",
+            "en" => $"Hello {patientName}, \nYour medical appointment is scheduled for {dateFormatted}.",
             _ => throw new ArgumentException("Unsupported language code. Only 'en' and 'es' are supported.")
         };
+    }
+
+    private string FormatDate(DateTimeOffset dateTime, string languageCode)
+    {
+        var culture = GetCulture(languageCode);
+
+        if (languageCode.ToLower() == "es")
+        {
+            // Example: "01 de mayo de 2025 a las 9:00"
+            return dateTime.ToString("dd 'de' MMMM 'de' yyyy 'a las' HH:mm", culture);
+        }
+
+        // Example: "Thursday, May 1, 2025 at 09:00"
+        return dateTime.ToString("f", culture);
     }
 
     private CultureInfo GetCulture(string languageCode)
